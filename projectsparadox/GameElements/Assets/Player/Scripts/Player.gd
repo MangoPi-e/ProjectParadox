330322cb2de87983158a6c43d6b7b8d252c97bcb
Mango <themango.2007@gmail.com>
2024-11-22 02:24:19 +0300

    Setting_Up_5

--- projectsparadox/GameElements/Assets/Player/Scripts/Player.gd
+++ projectsparadox/GameElements/Assets/Player/Scripts/Player.gd
@@ -2,7 +2,7 @@ class_name Player
 extends CharacterBody2D
 
 @onready
-var Anim = $Anim
+var Anim = $PlayerAnimManager
 @onready
 var StateMachine = $StateMachine
 var MovementDir = Vector2.ZERO
@@ -21,8 +21,8 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _physics_process(delta):
 	if Enabled:#Check if no cutscene is on
-		MovementDir#Getting the MovementDir
-		MovementDir = MovementDir.normalized()#Normalize to handle (1,1) to be (cos(theta), sin(theta))
+		MovementDir = Vector2(Input.get_action_strength("AnalogRight") - Input.get_action_strength("AnalogLeft"),Input.get_action_strength("AnalogDown") - Input.get_action_strength("AnalogUp"))#Getting the MovementDir
+		MovementDir = MovementDir.limit_length(1)#Normalize to handle (1,1) to be (cos(theta), sin(theta))
 		StateMachine.process_physics(delta)#Pass to the StateMachine
 
 func _process(delta):
@@ -33,6 +33,7 @@ func _process(delta):
 #Custom Functions
 func handle_movement(_delta , Speed):#Calculate the velocity based on the Speed and the MovementDir
 	velocity = MovementDir * Speed
+	print(MovementDir, Input.get_action_strength("AnalogRight"), velocity)
 	move_and_slide()#Apply velocity
 	
 	
--- projectsparadox/GameElements/Assets/Player/Scripts/idel.gd
+++ projectsparadox/GameElements/Assets/Player/Scripts/idel.gd
@@ -7,13 +7,12 @@ func enter() -> void:
 	super()
 
 func process_input(_event) -> State:
-	if Input.is_action_just_pressed("INTERACT"):
+	if Input.is_action_just_pressed("Interact"):
 		Parent.ObjectsInRange[0].player_says_work()
-	if Vector2(Input.get_axis('LEFT', 'RIGHT'), Input.get_axis('UP', 'DOWN')) != Vector2.ZERO and Parent.Enabled:
+	if Vector2(Input.get_action_strength("AnalogRight") - Input.get_action_strength("AnalogLeft"),Input.get_action_strength("AnalogUp") - Input.get_action_strength("AnalogDown")) != Vector2.ZERO and Parent.Enabled:
 		return MoveState
 	return null
 	
 
 func _process(delta: float) -> void:
-	if Global.InteractTime < Global.MaxInteractTime:
-		Global.InteractTime += delta
+	pass
