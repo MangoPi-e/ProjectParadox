906bfe6ac5cf99e70b1ee501719f93e9b762c870
Mango <124896261+Mohammed-Aljaafari@users.noreply.github.com>
2024-11-22 14:55:03 +0300

    menu update

--- projectsparadox/GameElements/Assets/UI/Scripts/MainMenu.gd
+++ projectsparadox/GameElements/Assets/UI/Scripts/MainMenu.gd
@@ -1,11 +1,44 @@
 extends Control
 
+@onready var panel = $Panel_1
+@onready var style_list = [
+	preload("res://GameElements/Assets/UI/menues/stayles/main_menu.tres"),
+	preload("res://GameElements/Assets/UI/menues/stayles/main_menu4.tres"),
+	preload("res://GameElements/Assets/UI/menues/stayles/main_menu2.tres"),
+	preload("res://GameElements/Assets/UI/menues/stayles/main_menu3.tres")
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Create and configure the Timer node
+	var timer = Timer.new()
+	timer.wait_time = 3.0  
+	add_child(timer)  
+	print("starting")
+	timer.timeout.connect(Callable(self, "_on_timer_timeout"))
+	timer.start()
+
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("ui_accept"):
+		print("....")
+	
+
+func _on_timer_timeout():
+	print("interd")
+	var rng = RandomNumberGenerator.new()
+	var num1 = rng.randi_range(0, 3)
+	while (Global.index==num1):
+		num1 = rng.randi_range(0, 3)
+	
+	print(num1)
+	print(style_list[num1])
+	var new_texture = style_list[num1] as StyleBoxTexture
+	var new_style = StyleBoxTexture.new()
+	new_style.texture = new_texture 
+	panel.add_theme_stylebox_override("panel", new_texture)
+	Global.index = num1
+	
--- projectsparadox/GameElements/Global.gd
+++ projectsparadox/GameElements/Global.gd
@@ -4,3 +4,5 @@ var TimeP:int = 0
 var Phase:int = 0
 var TimeMod: int = 1
 var PlayerObject:Player
+#index for menue
+var index: int =0
